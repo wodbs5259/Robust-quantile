e7a124e0ef65bec1477c50738e7168c84e270461
wodbs5259 <84220118+wodbs5259@users.noreply.github.com>
2025-04-05 10:39:54 +0900

    Update Spline.R

--- analysis/Spline.R
+++ analysis/Spline.R
@@ -75,6 +75,47 @@ for(i in 1:length(tau.tmp)){
 #         axis.title.y = element_text(size = 8, face='bold'),
 #         panel.grid = element_blank(),
 #         plot.margin = margin(t = 5, r = 15, b = 5, l = 5))
-# 
-# 
+#  
 # p12 <- gridExtra::grid.arrange(p1, p2, ncol = 2)
+ 
+## Residual plot and qqplot
+# balloon <- balloon[(1:length(balloon)) %% 10 == 1]
+# 
+# dat <- data.frame(x = seq_along(balloon), y = balloon)
+# 
+# check <- smooth.spline(x = dat$x, y = dat$y)
+# 
+# b_spline <- ns(seq_along(balloon), df = check$df)
+# Quantile <- coef(rq(y ~ ns(x, df = check$df), tau = 0.5, data = dat))
+# Quantile.y.pred <- drop(Quantile[1] + Quantile[-1] %*% t(b_spline))
+# 
+# fitted.values <- Quantile.y.pred
+# residuals <- dat$y - Quantile.y.pred
+# 
+# p3 <- ggplot(data.frame(pred = seq(0, 1, length.out = length(seq_along(balloon))), resid = residuals)) +
+#   geom_point(aes(x = pred, y = resid), size = 0.5) +
+#   geom_hline(yintercept = 0, lwd = 0.5) +
+#   labs(x = "Time", y = "Residuals", title = "Residual plot") +
+#   theme_bw() +
+#   theme(plot.title = element_text(size = 10, hjust = 0.5, face='bold'),
+#         axis.text.x = element_text(size = 6, face='bold'),
+#         axis.text.y = element_text(size = 6, hjust=1, face='bold'),
+#         axis.title.x = element_text(size = 8, face='bold'),
+#         axis.title.y = element_text(size = 8, face='bold'),
+#         panel.grid = element_blank(),
+#         plot.margin = margin(t = 5, r = 15, b = 5, l = 5))
+# 
+# p4 <- ggplot(data.frame(resid = residuals), aes(sample = resid)) +
+#   stat_qq(size = 0.5) +  
+#   stat_qq_line(color = "black") + 
+#   labs(x = "Theoretical Quantiles", y = "Sample Quantiles", title = "QQ plot") +
+#   theme_bw() +
+#   theme(plot.title = element_text(size = 10, hjust = 0.5, face='bold'),
+#         axis.text.x = element_text(size = 6, face='bold'),
+#         axis.text.y = element_text(size = 6, hjust=1, face='bold'),
+#         axis.title.x = element_text(size = 8, face='bold'),
+#         axis.title.y = element_text(size = 8, face='bold'),
+#         panel.grid = element_blank(),
+#         plot.margin = margin(t = 5, r = 15, b = 5, l = 5))
+# 
+# p34 <- gridExtra::grid.arrange(p3, p4, ncol = 2)
