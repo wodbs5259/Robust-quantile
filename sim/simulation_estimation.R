b6eb58ca162b5a227ca1311e65cb66ad7a66e744
wodbs5259 <84220118+wodbs5259@users.noreply.github.com>
2025-05-14 11:13:14 +0900

    Update simulation_estimation.R

--- sim/simulation_estimation.R
+++ sim/simulation_estimation.R
@@ -49,10 +49,15 @@ simulation.data.make <- function(n = 100, error.rate = 0.1, simul = 1) {
     real.data <- rnorm(n * (1 - error.rate), mean = 0, sd = 1)
     contaminate.data <- rnorm(n * error.rate, mean = 5, sd = 1)
     
-  } else {
+  } else if(simul == 2) {
     
     real.data <- rnorm(n * (1 - error.rate), mean = 0, sd = 1)
     contaminate.data <- rnorm(n * error.rate, mean = 0, sd = 5)
+    
+  } else { 
+  
+    real.data <- rt(n * (1 - error.rate), df = 3)/sqrt(3) # rt(n * (1 - error.rate), df = 5)/sqrt(5/3)
+    contaminate.data <- rnorm(n * error.rate, mean = 5, sd = 1)
     
   }
   
@@ -61,6 +66,8 @@ simulation.data.make <- function(n = 100, error.rate = 0.1, simul = 1) {
   return(list(real.data = real.data, data = data))
   
 }
+
+# simulation 1, 2
 
 n.tmp <- c(100, 200, 400)
 error.rate.tmp <- c(0.05, 0.1)
@@ -108,3 +115,51 @@ for(k in 1:nrow(param.grid)){
       " tau :", params[, "tau"], "finish\n\n")
   
 }
+
+# simulation 3
+
+n.tmp <- c(100, 200, 400)
+error.rate.tmp <- c(0.05, 0.1, 0.15, 0.2)
+simul.tmp <- c(3)
+tau.tmp <- c(0.95, 0.9, 0.75, 0.5, 0.25, 0.1, 0.05)
+
+param.grid <- expand.grid(n = n.tmp, error.rate = error.rate.tmp, simul = simul.tmp, tau = tau.tmp)
+
+for(k in 1:nrow(param.grid)){
+  params <- param.grid[k, ]
+  
+  cat("n :", params[, "n"], " error.rate :", params[, "error.rate"], " simul :", params[, "simul"], 
+      " tau :", params[, "tau"], "start\n\n")
+  
+  tau <- params[, "tau"]
+  
+  for (i in 1:100) {
+    
+    set.seed(13 * i + 120)
+    
+    tmp <- simulation.data.make(n = params[, "n"], error.rate = params[, "error.rate"], 
+                                simul = params[, "simul"])
+    
+    real.data <- tmp$real.data
+    data <- tmp$data
+    
+    # S-Quantile
+    S.Q.MADN <- fit.rob(data = data, tau = tau, eff = 0.95, sig_type = "MADN", Type = "Estimation", center = median(data))$m
+    
+    true.quantile <- as.numeric( qt(p = tau, df = 3)/sqrt(3) )
+    
+    Quantile <- as.numeric( quantile(data, probs = tau) )
+    
+    write(c(i, true.quantile, Quantile, S.Q.MADN),
+          file = paste0("../result/est/", "Simul", params[, "simul"], "/",
+                        "Simul", params[, "simul"], "_n_", params[, "n"], 
+                        "_error.rate_", params[, "error.rate"], "_tau_", params[, "tau"], ".txt"),
+          ncolumns = length(c(i, true.quantile, Quantile, S.Q.MADN)), append = T)
+    
+  }
+  
+  cat("n :", params[, "n"], " error.rate :", params[, "error.rate"], " simul :", params[, "simul"], 
+      " tau :", params[, "tau"], "finish\n\n")
+  
+}
+
